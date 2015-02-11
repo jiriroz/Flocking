4c02a0205555675ac706ccd82cd808a6db9d0e5f
jirkaroz <jiri.roznovjak@gmail.com>
2015-02-11 17:48:23 -0500

    Refactoring, added tails and a nice background

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ var HEIGHT = 600;
 document.getElementById('canvas').style.width = WIDTH;
 document.getElementById('canvas').style.height = HEIGHT;
 
+//size of one square for the bin-lattice spacial subdivision
 var RESOLUTION = 20;
+//all velocities are multiplied by this constant, use for scaling
 var VELOCITYSCALE = 0.9;
 
+//Vehicle that has methods for displaying itself, steering,
+//and pursuing/escaping a target.
 var Vehicle = function (x,y,size) {
     this.velocity = new Point(0,0);
     this.acceleration = new Point(0,0);
@@ -18,12 +22,13 @@ var Vehicle = function (x,y,size) {
     this.mass = 1;
     this.lastAngle = 0;
     this.size = size;
+    this.count = 0;
     this.display(x,y);
 };
 
 Vehicle.prototype.display = function (x,y) {
-    this.image = new Path.Circle([x,y],this.size);
-    this.image.fillColor = new Color(0,0,0);
+    this.body = new Path.Circle([x,y],this.size);
+    this.body.fillColor = new Color(0,0,0);
 };
 
 Vehicle.prototype.run = function () {
@@ -37,31 +42,50 @@ Vehicle.prototype.updateDirection = function () {
 
 Vehicle.prototype.update = function () {
     this.velocity += this.acceleration;
-    this.image.position += this.velocity;
+    this.body.position += this.velocity;
     this.acceleration *= 0;
 };
 
+//when the vehicle get beyond the screen edge, it
+//will appead on the other side
 Vehicle.prototype.checkEdges = function () {
-    if (this.image.position.x < 0) {
-        this.image.position.x = WIDTH;
-    } else if (this.image.position.x > WIDTH) {
-        this.image.position.x = 0;
+    if (this.body.position.x < 0) {
+        this.body.position.x = WIDTH;
+    } else if (this.body.position.x > WIDTH) {
+        this.body.position.x = 0;
     }
-    if (this.image.position.y < 0) {
-        this.image.position.y = HEIGHT;
-    } else if (this.image.position.y > HEIGHT) {
-        this.image.position.y = 0;
+    if (this.body.position.y < 0) {
+        this.body.position.y = HEIGHT;
+    } else if (this.body.position.y > HEIGHT) {
+        this.body.position.y = 0;
     }
 };
 
-Vehicle.prototype.applyBehaviors = function () {
+//calculates segments of the tail
+Vehicle.prototype.calculateTail = function (tail,origin) {
+    var segments = tail.segments;
+    var speed = this.velocity.length;
+    var pieceLength = 1 + speed / 2;
+    var point = origin;
+    // Chain goes the other way than the movement
+    var lastVector = -this.velocity;
+    for (var i = 0; i < segments.length; i++) {
+        var vector = segments[i].point - point;
+        this.count += speed * 100;
+        var wave = Math.sin((this.count + i * 3) / 200);
+        var sway = lastVector.rotate(90).normalize(wave);
+        point += lastVector.normalize(pieceLength) + sway;
+        segments[i].point = point;
+        lastVector = vector;
+    }
+    tail.smooth();
 };
 
 Vehicle.prototype.applyForce = function (force) {
     force /= this.mass;
     this.acceleration += force;
 };
-
+//this method makes change of direction look smoother
 Vehicle.prototype.steer = function (desired) {
     desired = desired.normalize(this.maxspeed);
     var steerForce = desired - this.velocity;
@@ -72,27 +96,34 @@ Vehicle.prototype.steer = function (desired) {
     return steerForce;
 };
 
+//steer towards a targer
 Vehicle.prototype.goTo = function (targetPos) {
-    var desired = targetPos - this.image.position;
+    var desired = targetPos - this.body.position;
     return this.steer(desired);
 };
 
+//steer away from a target
 Vehicle.prototype.goAwayFrom = function (targetPos) {
-    var desired = this.image.position - targetPos;
+    var desired = this.body.position - targetPos;
     return this.steer(desired);
 };
 
 Vehicle.prototype.isWithinDistance = function (obj,margin) {
-    var dist = (this.image.position - obj.image.position).length;
+    var dist = (this.body.position - obj.body.position).length;
     return dist < margin;
 };
 
+//In order to make the flocking more efficient, the canvas is divided
+//into so called bin-lattice spacial subdivision, which are many small
+//squares with the length of the side specified by the variable RESOLUTION.
+//This method takes the subdivision and returns all vehicles that are
+//contained within the vehicle's bin and in neighboring bins.
 Vehicle.prototype.getNeighbors = function (subdivision) {
     var x,y;
     var len = subdivision.length;
     var sublen = subdivision[0].length;
-    var col = parseInt(this.image.position.x / RESOLUTION);
-    var row = parseInt(this.image.position.y / RESOLUTION);
+    var col = parseInt(this.body.position.x / RESOLUTION);
+    var row = parseInt(this.body.position.y / RESOLUTION);
     col = limit(col,0,WIDTH/RESOLUTION - 1);
     row = limit(row,0,HEIGHT/RESOLUTION - 1);
     var neighborhood = [[1,0],[-1,0],[0,1],[0,-1],[0,0],
@@ -108,6 +139,8 @@ Vehicle.prototype.getNeighbors = function (subdivision) {
     return neighbors;
 };
 
+//Prey that inherits from Vehicle and in addition has the ability
+//to flock.
 var Prey = function (x,y) {
     Vehicle.call(this,x,y,3);
     this.maxspeed = 7*VELOCITYSCALE;
@@ -116,20 +149,37 @@ var Prey = function (x,y) {
 
 Prey.prototype = Object.create(Vehicle.prototype);
 
-Prey.prototype.display = function (x,y) {
-    this.image = new Path.Circle([x,y],this.size);
-    var r = Math.random()*0.3;
-    var g = Math.random()*0.3;
-    var b = Math.random()*0.3;
-    this.image.fillColor = new Color(r,g,b);
+Prey.prototype.run = function () {
+    Vehicle.prototype.run.call(this);
+    this.calculateTail(this.tail,this.body.position);
 };
 
+Prey.prototype.display = function (x,y) {
+    var color = new Color(Math.random()*0.3,
+        Math.random()*0.3,
+        Math.random()*0.3);
+    this.body = new Path.Circle({
+        center: [x, y],
+        radius: this.size,
+        fillColor: color
+    });
+    this.tail = new Path({
+        strokeColor: color,
+        strokeWidth: 2,
+        strokeCap: 'round'
+    });
+    for (var i = 0; i < 5; i++) {
+        this.tail.add(new Point());
+    }
+};
+
+//applies flocking rules and escaping from the predators
 Prey.prototype.applyBehaviors = function (flock,predators) {
     this.applyRules(flock);
     var sumEscape = new Point(0,0);
     for (var i=0; i<predators.length; i++) {
-        if (this.isWithinDistance(predators[i],100)) {
-            var escape = this.goAwayFrom(predators[i].image.position);
+        if (this.isWithinDistance(predators[i],120)) {
+            var escape = this.goAwayFrom(predators[i].body.position);
             escape *= 3;
             sumEscape += escape;
         }
@@ -140,23 +190,24 @@ Prey.prototype.applyBehaviors = function (flock,predators) {
     }
 };
 
+//applies the rules for flocking: separation, alignment, cohesion
 Prey.prototype.applyRules = function (flock) {
     var flockArray = this.getNeighbors(flock.subdivision);
     var countSeparation = 0;
     var countAlignment = 0;
     var neighborhood = 100;
-    var desiredSeparation = 30;
+    var desiredSeparation = 40;
     var velocities = new Point(0,0);
     var separate = new Point(0,0);
     for (var i=0; i<flockArray.length; i++) {
-        var dist = this.image.position.getDistance(flockArray[i].image.position);
+        var dist = this.body.position.getDistance(flockArray[i].body.position);
             if (dist > 0) {
                 if (dist < neighborhood) {
                     velocities += flockArray[i].velocity;
                     countAlignment++;
                 }
                 if (dist < desiredSeparation) {
-                var diff = this.image.position-flockArray[i].image.position;
+                var diff = this.body.position-flockArray[i].body.position;
                 diff = diff.normalize();
                 diff /= dist;
                 separate += diff;
@@ -183,17 +234,50 @@ Prey.prototype.applyRules = function (flock) {
     }
 };
 
+//predator object that inherits from Vehicle and has the ability to
+//chase prey.
 var Predator = function (x,y) {
     Vehicle.call(this,x,y,7);
-    this.maxspeed = 10*VELOCITYSCALE;
+    this.maxspeed = 12*VELOCITYSCALE;
     this.maxsteer = 0.4*VELOCITYSCALE;
 };
 
 Predator.prototype = Object.create(Vehicle.prototype);
 
+Predator.prototype.run = function () {
+    Vehicle.prototype.run.call(this);
+    this.calculateTail(this.mainTail,this.body.position);
+    var offset = this.velocity.rotate(90).normalize(6);
+    this.calculateTail(this.leftTail,this.body.position - offset);
+    this.calculateTail(this.rightTail,this.body.position + offset);
+};
+
 Predator.prototype.display = function (x,y) {
-    this.image = new Path.Circle([x,y],this.size);
-    this.image.fillColor = new Color(1,0,0);
+    this.body = new Path.Circle([x,y],this.size);
+    this.body.fillColor = new Color(1,0,0);
+    
+    this.mainTail = new Path({
+        strokeColor: "red",
+        strokeWidth: 3,
+        strokeCap: "round"
+    });
+    this.leftTail = new Path({
+        strokeColor: "red",
+        strokeWidth: 2,
+        strokeCap: "round"
+    });
+    this.rightTail = new Path({
+        strokeColor: "red",
+        strokeWidth: 2,
+        strokeCap: "round"
+    });
+    for (var i = 0; i < 7; i++) {
+        this.mainTail.add(new Point());
+        if (i > 2) {
+            this.leftTail.add(new Point());
+            this.rightTail.add(new Point());
+        }
+    }
 };
 
 Predator.prototype.applyBehaviors = function (flock) {
@@ -205,7 +289,7 @@ Predator.prototype.findNearest = function (flock) {
     var flockArray = flock.flock;
     var nearest = [10000,-1];
     for (var i=0; i<flockArray.length; i++) {
-        var dist = this.image.position.getDistance(flockArray[i].image.position);
+        var dist = this.body.position.getDistance(flockArray[i].body.position);
         if (dist < nearest[0]) {
             nearest[0] = dist;
             nearest[1] = i;
@@ -214,7 +298,7 @@ Predator.prototype.findNearest = function (flock) {
     if (nearest[1] === -1) {
         return new Point(0,0);
     }
-    return this.goTo(flockArray[nearest[1]].image.position);
+    return this.goTo(flockArray[nearest[1]].body.position);
 };
 
 Predator.prototype.isEating = function (prey) {
@@ -222,6 +306,8 @@ Predator.prototype.isEating = function (prey) {
     return this.isWithinDistance(prey,margin);
 };
 
+//collection of prey objects that allows to apply flocking behaviors
+//to them.
 var Flock = function (count) {
     this.flock = [];
     this.center = new Point(0,0);
@@ -235,11 +321,12 @@ Flock.prototype.createFlock = function (count) {
     }
 };
 
+//Assigns each prey to a spacial subdivision
 Flock.prototype.assignToSubsets = function () {
     this.createEmptySubdivision();
     for (var i=0; i<this.flock.length; i++) {
-        var col = parseInt(this.flock[i].image.position.x/RESOLUTION);
-        var row = parseInt(this.flock[i].image.position.y/RESOLUTION);
+        var col = parseInt(this.flock[i].body.position.x/RESOLUTION);
+        var row = parseInt(this.flock[i].body.position.y/RESOLUTION);
         col = limit(col,0,WIDTH/RESOLUTION - 1);
         row = limit(row,0,HEIGHT/RESOLUTION - 1);
         this.subdivision[col][row].push(this.flock[i]);
@@ -265,8 +352,10 @@ Flock.prototype.run = function (predators) {
         this.flock[i].run();
         for (var j=0; j<predatorArray.length; j++) {
             if (predatorArray[j].isEating(this.flock[i])) {
-                this.flock[i].image.remove();
+                this.flock[i].body.remove();
+                this.flock[i].tail.remove();
                 this.flock.splice(i,1);
+                createRandomBoid(Prey,this.flock);
                 break;
             }
         }
@@ -276,13 +365,15 @@ Flock.prototype.run = function (predators) {
 Flock.prototype.computeAvgPosition = function () {
     var avgPos = new Point(0,0);
     for (var i=0; i<this.flock.length; i++) {
-        avgPos += this.flock[i].image.position;
+        avgPos += this.flock[i].body.position;
     }
     if (this.flock.length > 0) {
         this.center = avgPos/this.flock.length;
     }
 };
 
+//group of predators. Doesn't add any new functionality,
+//only makes the coding easier.
 var Predators = function (count) {
     this.predators = [];
     this.createPredators(count);
@@ -301,6 +392,7 @@ Predators.prototype.run = function (flock) {
     }
 };
 
+//bounds of the cavnas
 var Bounds = function () {
     this.path = new Path();
     this.path.strokeColor = 'black';
@@ -314,9 +406,8 @@ Bounds.prototype.remove = function () {
 };
 
 var Background = function () {
-    var rectangle = new Rectangle(new Point(0,0), new Point(WIDTH,HEIGHT));
-    this.path = new Path.Rectangle(rectangle);
-    this.path.fillColor = new Color(0.5,0.8,0.9);
+    this.image = new Raster("img/sea_texture.jpg");
+    this.image.position = [WIDTH/2,HEIGHT/2];
 };
 
 var createRandomBoid = function (Type,array) {
